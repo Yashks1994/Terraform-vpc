2c28dbf77ada2be35c74a27a20a90043276868be
Yash Kumar Singh <yks1994@gmail.com>
2020-04-27 15:39:17 +0530

    Second Commit

---
+++ test5.py
@@ -0,0 +1,11 @@
+a = [5,2,6,4,6,5,-6,26,45,2,56,623]
+
+for i in range(len(a)):
+    for j in range(i+1, len(a)):
+        if a[i] > a[j]:
+            a[i], a[j] = a[j], a[i]
+
+https://yash.com
+print(a)
+
+Localhost
